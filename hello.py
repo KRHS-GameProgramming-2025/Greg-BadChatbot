35db3fb17b0ccd4b020d6a63a89c0ccf8d26fb16
Greg <greg.huang@kearsarge.org>
2024-09-10 10:14:37 -0400

    setup

---
+++ hello.py
@@ -0,0 +1,36 @@
+name = input("what is your name?")
+greeting = "I know where you live"
+print(greeting + " " + name)
+if name == "Dave":
+    print("Long time no see")
+food = input("What is your favourite food?")
+if food == "fries":
+    print("Nice! I am a big fan of potato!")
+if food == "Fries":
+    print("Nice! I am a big fan of potato!")
+else:
+    print("I like to eat brains!🧠")
+game = input("what is your favourite game?")
+if game == "monster hunter":
+    print("Me too :)))")
+elif game == "Monster Hunter":
+    print("Me too :)))")
+elif game == "Plants vs Zombies":
+    print("can I eat your brains?🧠")   
+else: 
+    print("Interesting.")
+song = input("what is your favourote song?")
+if song == "Daisy Bell":
+    print("That is my favourite song too!🔴")
+elif song == ("daisy bell"):
+    print("That is my favourite song too!🔴")
+else:
+    print("(´⊙ω⊙`)ᵒᵐᵍᵎᵎᵎ my favourite is 𝓓𝓪𝓲𝓼𝔂 𝓑𝓮𝓵𝓵. I can sing it for you!     ▶︎ •၊၊||၊|။||||။‌‌‌‌‌၊|• 0:10")
+Answer = input("Can I send you to the moon?")
+if Answer == "Yes":
+    print(" suffocate "*999)
+elif Answer == "No":
+    print(":(")
+else:
+    print(" Kill your self "*999)
+
